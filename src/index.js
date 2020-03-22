32977a75c656d82f349b68893f2bf1eae71e065d
Linh Le <lvlinh96@gmail.com>
2020-03-22 15:56:20 +0700

    [new] add bad object

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ String.prototype.isQuestion = function isQuestion() {
     /* Returns true if the last letter of the input string a question mark. 
     false otherwise
     */
+   const object = new Object();
    const questionable = new RegExp(/\?$/);
    return questionable.test(this);
 };
